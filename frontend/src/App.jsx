da555cd0842989e5b2daaf6dd47c224fcb83d330
ummfieg <ummfieg@naver.com>
2026-06-08 19:12:53 +0900

    chore: React-FastAPItest 연결 확인

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  const handleClick = async () => {
+    const res = await fetch("http://localhost:8000/health");
+    const data = await res.json();
+
+    setMessage(data.status);
+  };
+
   return (
     <>
       <h1>AI Font Recommendation</h1>
 
       <input placeholder="브랜드 설명 입력" />
-      <button>추천받기</button>
+      <button onClick={handleClick}>추천받기</button>
 
-      <div>추천 결과 영역</div>
+      <div>
+        추천 결과 영역
+        <p>{message}</p>
+      </div>
     </>
   );
 }
