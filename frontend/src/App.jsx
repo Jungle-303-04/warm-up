a0d66397e210b4c41aa13e2c0cbc60b438637549
ummfieg <ummfieg@naver.com>
2026-06-08 14:17:57 +0900

    chore: React + Vite 초기 설정

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>AI Font Recommendation</h1>
+
+      <input placeholder="브랜드 설명 입력" />
+      <button>추천받기</button>
+
+      <div>추천 결과 영역</div>
+    </>
+  );
+}
+
+export default App;
