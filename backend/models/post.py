e430e2bbe9541a7c6217a33f229fc07c518b7a45
ummfieg <ummfieg@naver.com>
2026-06-10 21:57:35 +0900

    feat: post 테이블 컬럼 정의

---
+++ backend/models/post.py
@@ -0,0 +1,18 @@
+from typing import Optional
+from datetime import datetime, timezone
+
+from sqlmodel import SQLModel, Field
+from sqlalchemy import Column,String, Text
+
+class Post(SQLModel, table=True):
+    __tablename__ = "posts"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    title: str = Field(sa_column=Column(String(100), nullable=False))
+    content: str = Field(sa_column=Column(Text, nullable=False))
+
+    font_id: int = Field(foreign_key="fonts.id")
+
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
