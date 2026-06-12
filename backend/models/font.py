0143b7a73748874e7a82c25d049aa1d16ad684d1
ummfieg <ummfieg@naver.com>
2026-06-12 10:27:13 +0900

    refactor: font 데이터 변경에 따른 DB 컬럼 수정 license_summary 및 webfonts , download_url추가, weights 및 tags타입변경

--- backend/models/font.py
+++ backend/models/font.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from sqlmodel import SQLModel, Field
-from sqlalchemy import Column, String, Text
+from sqlalchemy import Column, String, Text, JSON
+
 
 class Font(SQLModel, table=True):
     __tablename__ = "fonts"
@@ -11,12 +12,16 @@ class Font(SQLModel, table=True):
     source: str = Field(sa_column=Column(String(50), nullable=False))
     is_paid: bool = Field(default=False, nullable=False)
 
-    license: str = Field(sa_column=Column(String(100), nullable=False))
-    category: str = Field(sa_column=Column(String(50), nullable=False))
+    license: str | None = Field(default=None, sa_column=Column(Text))
+   
+    category: str | None = Field(default=None, sa_column=Column(String(50), nullable=True))
 
-    tags: str = Field(sa_column=Column(Text, nullable=True))
-    description: str = Field(sa_column=Column(Text, nullable=True))
-    weights: str = Field(sa_column=Column(Text, nullable=True))
+    tags: list[str] = Field(default_factory=list, sa_column=Column(JSON))
+    description: str | None = Field(default=None, sa_column=Column(Text, nullable=True))
+    weights: list[int] = Field(default_factory=list,sa_column=Column(JSON))
 
-    webfont_url: str = Field(sa_column=Column(Text, nullable=True))
+    download_url : str | None = Field(default=None, sa_column=Column(Text))
     source_url: str = Field(sa_column=Column(Text, nullable=False))
+
+    license_summary: list[dict] = Field(default_factory=list, sa_column=Column(JSON))
+    webfonts: list[dict] = Field(default_factory=list, sa_column=Column(JSON))
