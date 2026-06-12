4c68c7eea4a3a18e096e2768bf4e499d4f777818
ummfieg <ummfieg@naver.com>
2026-06-12 17:35:46 +0900

    feat: 이미 보유하고 있는 font data 저장 방지 기능 구현

---
+++ backend/tests/test_noonnu.py
@@ -0,0 +1,27 @@
+from crawler.noonnu import scrape_font_detail
+from models.font import Font
+from sqlmodel import Session,select
+from database import engine
+
+
+url = "https://noonnu.cc/font_page/694"
+
+font_data = scrape_font_detail(url)
+with Session(engine) as session:
+    # 폰트 보유 여부 조회
+    existing_font = session.exec(
+        select(Font).where(
+            Font.source_url == font_data["source_url"]
+        )
+    ).first()
+
+    if existing_font:
+        print("이미 저장된 폰트입니다.")
+    
+    else:
+        font = Font(**font_data)
+
+        session.add(font)
+        session.commit()
+
+        print("저장 완료")
